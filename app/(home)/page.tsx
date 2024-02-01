aee68bd108975cd5702fd05e4f4b3d221b643805
Victor Queiroga <victorqueiroga.jp@gmail.com>
2024-02-01 16:28:02 -0300

    feat: header parte I

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
+import Header from "../_components/header";
 
 export default function Home() {
-  return <h1>Teste</h1>;
+  return (
+    <Header />
+  );
 }
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
-import { Card } from "./ui/card";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
 
 const Header = () => {
     return (
         <Card>
-            <Image src="/logo.png" alt="logo barbearia" height={22} width={120}></Image>
+            <CardContent>
+                <Image src="/logo.png" alt="logo barbearia" height={22} width={120}></Image>
+                <Button variant={"outline"} size={"icon"}>
+                    <MenuIcon />
+                </Button>
+            </CardContent>
         </Card>
     );
 }
